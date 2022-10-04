46bee8f34a1644d26d642836919bb512aae256d4
thamdi1924 <55790585+thamdi1924@users.noreply.github.com>
2022-10-04 23:27:13 +0300

    Update nsg.tf
    fixed many things in NSG

--- nsg.tf
+++ nsg.tf
@@ -1,85 +1,30 @@
-resource "azurerm_network_security_group" "nsg-dotnet" {
-  name                = "nsg-dotnet-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-
-  security_rule {
-    name                       = "js-dotnet"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "10.2.0.32/27"
-    destination_address_prefix = "10.2.0.0/27"
-  }
-
-  security_rule {
-    name                       = "dotnet-js"
-    priority                   = 100
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "10.2.0.0/27"
-    destination_address_prefix = "10.2.0.32/27"
-  }
-
-  security_rule {
-    name                       = "js"
-    priority                   = 250
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "3000"
-    destination_port_range     = "3000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 300
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "22"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
 resource "azurerm_network_security_group" "nsg-js" {
   name                = "nsg-js-${var.environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "dotnet-js"
+    name                       = "dotnet-to-js"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = "10.2.0.0/27"
-    destination_address_prefix = "10.2.0.32/27"
+    source_address_prefix      = "10.2.1.0/24"
+    destination_address_prefix = "10.2.0.0/24"
   }
 
   security_rule {
-    name                       = "js-dotnet"
+    name                       = "js-to-dotnet"
     priority                   = 100
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = "10.2.0.32/27"
-    destination_address_prefix = "10.2.0.0/27"
+    source_address_prefix      = "10.2.0.0/24"
+    destination_address_prefix = "10.2.1.0/24"
   }
 
   security_rule {
@@ -88,7 +33,7 @@ resource "azurerm_network_security_group" "nsg-js" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "443"
+    source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
@@ -100,12 +45,93 @@ resource "azurerm_network_security_group" "nsg-js" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "22"
+    source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
+
+
+resource "azurerm_network_security_group" "nsg-dotnet" {
+  name                = "nsg-dotnet-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+
+  security_rule {
+    name                       = "js-to-dotnet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.2.0.0/24"
+    destination_address_prefix = "10.2.1.0/24"
+  }
+
+  security_rule {
+    name                       = "dotnet-to-js"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.2.1.0/24"
+    destination_address_prefix = "10.2.0.0/24"
+  }
+
+  security_rule {
+    name                       = "js"
+    priority                   = 250
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "js-to-sql"
+    priority                   = 200
+    direction                  = "outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.2.1.0/24"
+    destination_address_prefix = "10.2.2.0/24"
+  }
+
+  security_rule {
+    name                       = "sql-to-js"
+    priority                   = 200
+    direction                  = "inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.2.2.0/24"
+    destination_address_prefix = "10.2.1.0/24"
+  }
+}
+
+
 
 resource "azurerm_network_security_group" "nsg-sql" {
   name                = "nsg-sql-${var.environment}"
@@ -125,27 +151,27 @@ resource "azurerm_network_security_group" "nsg-sql" {
   }
 
   security_rule {
-    name                       = "sql-dotnet"
+    name                       = "sql-to-dotnet"
     priority                   = 100
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = "10.2.0.64/27"
-    destination_address_prefix = "10.2.0.32/27"
+    source_address_prefix      = "10.2.2.0/24"
+    destination_address_prefix = "10.2.1.0/24"
   }
 
     security_rule {
-    name                       = "dotnet-sql"
+    name                       = "dotnet-to-sql"
     priority                   = 200
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = "10.2.0.32/27"
-    destination_address_prefix = "10.2.0.64/27"
+    source_address_prefix      = "10.2.1.0/24"
+    destination_address_prefix = "10.2.2.0/24"
   }
 
     security_rule {
@@ -154,7 +180,7 @@ resource "azurerm_network_security_group" "nsg-sql" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "22"
+    source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
